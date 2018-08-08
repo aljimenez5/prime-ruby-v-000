096a83bf803ada3747e22df281792bf96a468445
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 02:58:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,5 +4,9 @@ def prime?(integer)
   lesser_integers.map do |lesser_int| 
     if integer % lesser_int == 0
       divisible_by << lesser_int
+      if divisible_by.count > 2
+        return false
+      else
+        return true
   true
 end
