500e61973a1346d603ef26c1e926952a1fef05d8
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 03:00:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(integer)
   lesser_integers = (1..integer).to_a
   divisible_by = []
-  lesser_integers.map {|lesser_int| divisible_by << lesser_int if integer % lesser_int == 0
-      divisible_by << lesser_int
-      if divisible_by.count > 2
+  lesser_integers.map {|lesser_int| divisible_by << lesser_int if integer % lesser_int == 0}
+    if divisible_by.count > 2
         return false
       else
         return true
