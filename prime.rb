20987e8cf97ec1b6d373bd42bb9de0c57510aee6
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 02:36:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(integer)
   while integer > 1
-    if integer % integer == 0 && integer % 1
+    if integer % (2..integer) == 0
       return false
+    if integer % integer == 0 && integer % 1 == integer
+      return true
     else
       return true
     end
