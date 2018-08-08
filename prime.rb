daff53accc98fa4463f30c41b55125e03ae869ee
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 02:47:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 def prime?(integer)
   lesser_integers = (2..integer-1).to_a
   while integer > 0
-    lesser_integers.map do |lesser_int|
-      if integer % lesser_int == 0 || integer <= -1
-        return false
-      else
-        return true
+    lesser_integers.map {|lesser_int| return false if integer % lesser_int == 0 || integer <= -1}
+    true
       end
     end
   end
